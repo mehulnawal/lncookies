f8811b23c531c4b8599fd02e565873c0b6c8fd50
Mehul Nawal <your.email@example.com>
2024-10-09 23:59:02 +0530

    Done

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"], // This will apply Tailwind to all HTML and JS files across all folders
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
