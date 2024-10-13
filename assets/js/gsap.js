f2cf0a4dea8cfe6eda03e99f1ba18e14004b1ba5
Mehul Nawal <your.email@example.com>
2024-10-13 17:15:54 +0530

    Done

--- assets/js/gsap.js
+++ assets/js/gsap.js
@@ -6,55 +6,78 @@
 //     smooth: true,
 // })
 
-// // var main = document.querySelector("main")
-// // const scroll2 = new LocomotiveScroll({
-// //     el: main,
-// //     smooth: true,
-// // })
+// var main = document.querySelector(".main")
+// const scroll = new LocomotiveScroll({
+//     el: main,
+//     smooth: true,
+//     lerp: 0.8,
+// })
 
-// // var footer = document.querySelector("footer")
-// // const scroll3 = new LocomotiveScroll({
-// //     el: footer,
-// //     smooth: true,
+// window.addEventListener('load', () => {
+//     scroll2.update();
+// });
+
+// var footer = document.querySelector("footer")
+// const scroll3 = new LocomotiveScroll({
+//     el: footer,
+//     smooth: true,
 // })
 
 
 // scrollTrigger
+
 gsap.to('.single-cookie1', {
-    top: '10%',
+    marginLeft: '-10%',
+    marginTop: '-10%',
     duration: 2,
     scrollTrigger: {
-        trigger: '.single-cookie1',
+        trigger: '.single-cookie4',
         scroller: 'body',
         // markers: true,
         start: 'top 30%',
-        end: 'top 10%',
-        scrub: 3,
+        end: 'top 20%',
+        scrub: 2,
     }
 })
 
 gsap.to('.single-cookie2', {
-    top: '10%',
+    marginLeft: '-10%',
+    marginTop: '15%',
     duration: 2,
     scrollTrigger: {
-        trigger: '.single-cookie1',
+        trigger: '.single-cookie2',
         scroller: 'body',
         // markers: true,
-        start: 'top 30%',
-        end: 'top 10%',
-        scrub: 3,
+        start: 'top 50%',
+        end: 'top 30%',
+        scrub: 2,
     }
 })
 
 gsap.to('.single-cookie3', {
-    top: '10%',
+    marginLeft: '20px',
+    marginTop: '-15%',
     duration: 2,
     scrollTrigger: {
-        trigger: '.single-cookie1',
+        trigger: '.single-cookie3',
         scroller: 'body',
         // markers: true,
         start: 'top 30%',
-        end: 'top 10%',
-        scrub: 3,
+        end: 'top 20%',
+        scrub: 2,
+    }
+})
+
+gsap.to('.single-cookie4', {
+    marginLeft: '20px',
+    marginTop: '7%',
+    duration: 2,
+    scrollTrigger: {
+        trigger: '.single-cookie4',
+        scroller: 'body',
+        // markers: true,
+        start: 'top 45%',
+        end: 'top 30%',
+        scrub: 2,
     }
 })
